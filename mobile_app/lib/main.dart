e0cf8baecd2572578f86ef2db1a1cde6995480a0
Himasnhu-AT <aristech.official@gmail.com>
2023-09-21 22:19:10 +0530

    upload skleton on user-dashboard and form

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: const SplashScreen(),
-      home: const HomeScreen(),
+      home: const SplashScreen(),
     );
   }
 }
